425d9df6719ff5595a17b9c0132ef58780a403b1
carlos <carciniegas@knights.ucf.edu>
2023-07-18 06:16:03 -0400

    Add new print statement

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, this is a tutorial for git"); 
+        System.out.println("More info here");   
+    }
+}
